c2dd668bf4ade50d1d31910e4f5f3c842fe0e742
Hoc Nguyen Viet <hoc.nguyenviet90@gmail.com>
2020-03-01 18:10:34 +0700

    Never re-render child component
    set function param in react memo always is true

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React, { useState, useCallback } from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
   console.log("render parent");
   let [count, setCount] = useState(0);
   // State for our counter
-  const onClick = useCallback(() => {
-    setCount(count++);
-  }, []);
   return (
     <div>
       <div>{count}</div>
-      <Child onClick={onClick} />
+      <Child
+        onClick={e => {
+          console.log(count);
+          count++;
+          setCount(count);
+        }}
+      />
     </div>
   );
 }
 
-const Child = React.memo(function({ onClick }) {
-  console.log("render child");
-  return <button onClick={onClick} />;
-});
+const Child = React.memo(
+  function({ onClick }) {
+    console.log("render child");
+    return <button onClick={onClick} />;
+  },
+  () => {
+    return true;
+  }
+);
